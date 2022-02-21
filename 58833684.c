1720bbff8c33097e64209f3edefd2bfa4d85dba7
Bhumika Nayak <96485896+BhumikaNayak@users.noreply.github.com>
2022-02-22 02:40:15 +0530

    Add files via upload

---
+++ 58833684.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char C;
+    scanf("%c\n",&C);
+    
+    if( C=='A' || C=='E' || C=='I' || C=='O' || C=='U' )
+    {
+        printf("Vowel\n");
+    }
+    else
+    {
+        printf("Consonant\n");
+    }
+    
+    return 0;
+}
+//code by bhumika nayak
